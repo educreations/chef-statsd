12a8c8d9c0ffed38d96ee9fd9ae7109493b3d776
Chris Streeter <chris@chrisstreeter.com>
2014-01-02 10:52:42 -0800

    More stuff, some testing

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,10 @@
-maintainer        "Educreations, Inc."
-maintainer_email  "chris@educreations.com"
-license           "Apache 2.0"
-description       "Sets up an environment for statsd instances."
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version           "0.0.1"
-suggests          "ruby"
-
-recipe "default", "Sets up statsd to collect and forward to graphite."
+name                "statsd"
+maintainer          "Educreations, Inc."
+maintainer_email    "chris@educreations.com"
+license             "Apache 2.0"
+description         "Sets up an environment for statsd instances."
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version             "0.0.1"
 
 %w{ debian ubuntu }.each do |os|
   supports os
