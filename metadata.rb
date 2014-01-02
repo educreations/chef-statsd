77317a72012da480118ed4988c0672e0f0745356
Chris Streeter <chris@chrisstreeter.com>
2014-01-02 11:03:52 -0800

    Read the correct readme file

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer          "Educreations, Inc."
 maintainer_email    "chris@educreations.com"
 license             "Apache 2.0"
 description         "Sets up an environment for statsd instances."
-long_description    IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version             "0.0.1"
 
 %w{ debian ubuntu }.each do |os|
