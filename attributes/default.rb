22d3c9209cf38745a86d3a8e1c28b7d363e17d1f
Chris Streeter <chris@chrisstreeter.com>
2014-01-02 10:54:13 -0800

    Attributes cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,19 +3,15 @@
 # Attributes:: statsd
 #
 
-default[:statsd][:user] = 'www-data'
-default[:statsd][:group] = 'www-data'
+default[:statsd][:user] = 'statsd'
+default[:statsd][:group] = 'statsd'
 
 default[:statsd][:base] = '/opt/statsd'
 default[:statsd][:config] = 'config.js'
 
+default[:statsd][:source_url] = 'git://github.com/etsy/statsd.git'
+default[:statsd][:source_reference] = 'v0.7.0'
+
 default[:statsd][:port] = 8125
 default[:statsd][:graphite_port] = 2003
-default[:statsd][:graphite_host] = "localhost"
-
-default[:statsd][:user] = 'statsd'
-default[:statsd][:group] = 'statsd'
-
-default[:statsd][:source_url] = "git://github.com/etsy/statsd.git"
-default[:statsd][:source_reference] = 'v0.7.0'
-default[:statsd][:tmp_dir] = "/tmp"
+default[:statsd][:graphite_host] = 'localhost'
