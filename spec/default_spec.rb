1d5f75eb2d9fe6c35cfa0f6b7b1ec516aa1ab21a
Chris Streeter <chris@chrisstreeter.com>
2014-01-02 11:31:35 -0800

    Hopefully fix the default spec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,7 +6,23 @@ describe 'statsd::default' do
     runner.converge(described_recipe)
   end
 
-  it 'should include the statsd recipe' do
-    expect(chef_run).to include_recipe('statsd')
+  it 'should include the git recipe' do
+    expect(chef_run).to include_recipe('git')
+  end
+
+  it 'should include the nodejs recipe' do
+    expect(chef_run).to include_recipe('nodejs')
+  end
+
+  it 'should include the supervisor recipe' do
+    expect(chef_run).to include_recipe('supervisor')
+  end
+
+  it('should create /opt/statsd directory') do
+    expect(chef_run).to create_directory('/opt/statsd')
+  end
+
+  it('should create /opt/statsd/config.js') do
+    expect(chef_run).to render_file('/opt/statsd/config.js')
   end
 end
